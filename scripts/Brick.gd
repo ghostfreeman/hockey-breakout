646613e8183a0884c02fdce52a95d403dece1520
Cameron Kilgore <ghostfreeman@gmail.com>
2018-04-21 12:06:20 -0500

    breakpoint

--- scripts/Brick.gd
+++ scripts/Brick.gd
@@ -3,10 +3,17 @@ extends StaticBody2D
 onready var area2d = get_node("Area2D")
 
 func _ready():
+	print("Hello")
 	#connect the signal to a callback function
-	area2d.connect("body_enter", self, "_on_brick_body_enter")
+	area2d.connect("body_entered", self, "_on_brick_body_enter")
 
 func _on_brick_body_enter(body):
 	#if the ball hit the brick, destroy it
 	if body.get_name() == "Puck":
 		queue_free()
+
+func _on_Area2D_body_entered(body):
+	print("Collision (body_entered)")
+
+func _on_Area2D_body_shape_entered(body_id, body, body_shape, area_shape):
+	print("Collision (body_shape_entered)")
