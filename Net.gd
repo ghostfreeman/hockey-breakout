70def584b3ef4e9f3a01970a00540250c93b7996
Cameron Kilgore <ghostfreeman@gmail.com>
2018-04-21 18:55:35 -0500

    Fixed some collision issues

--- Net.gd
+++ Net.gd
@@ -1,7 +1,8 @@
 extends StaticBody2D
 
 func _on_Area2D_body_entered(body):
-	# Stop the puck
-	print("Puck in net, game over")
-	get_parent().get_node("Puck").hide()
-	get_parent().get_parent().get_node("Modal").show()
+	if body.get_name() == "Puck":
+		# Stop the puck
+		print("Puck in net, game over")
+		get_parent().get_node("Puck").hide()
+		get_parent().get_parent().get_node("Modal").show()
